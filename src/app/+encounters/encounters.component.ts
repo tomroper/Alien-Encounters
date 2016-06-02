472bab1e00ae0faf5ba18a6ad5eb2434a0298eef
Tom Roper <Tom@MacBook-Pro.local>
2016-06-02 16:32:50 -0700

    encounters page pulling data from API

--- src/app/+encounters/encounters.component.ts
+++ src/app/+encounters/encounters.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
+
+import { Encounter } from '../shared/models';
+import { EncounterService } from '../shared/services';
 
 @Component({
   moduleId: module.id,
   selector: 'app-encounters',
   templateUrl: 'encounters.component.html',
-  styleUrls: ['encounters.component.css']
+  styleUrls: ['encounters.component.css'],
+  providers: [EncounterService]
 })
 export class EncountersComponent implements OnInit {
+  
+  public encounters: Encounter[];
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    private encounterService: EncounterService
+  ) {}
 
   ngOnInit() {
+    console.log('encountersPage');
+    
+    this.encounterService.getEncounters().then( ( response => this.encounters = response) );
+    console.log(this.encounters);
+  }
+  
+  clicked(event) {
+    this.router.navigate(['/report'])
   }
 
 }
